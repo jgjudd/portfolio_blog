9bf7afd099c3b362c73960e51441e5f45fb62f25
Justin Judd <justinjudd@Justins-Mac-mini.local>
2019-07-27 22:53:25 -0400

    "Start django project, add example view"

---
+++ v-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/justinjudd/Desktop/Python_Folder/Portfolio_Blog/v-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
